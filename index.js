0102bbabb351c40af479fd8af637173fbc762437
Dan Blake <danielblake@lmo.com>
2017-08-14 12:24:07 -0400

    Changed let to var to fix minification issues

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 global.scrollDebounce = {
- 	fire:[],
- 	$the_window:'',
-	pause:100,
-	didScroll:false,
- 	addEvent:function(func, params){
- 		this.fire.push([func, params]);
- 	},
-	runFunctions:function(){
-		for(let i=0; i<scrollDebounce.fire.length; i++){
+	fire: [],
+	pause: 100,
+	didScroll: false,
+	addEvent: function(func, params) {
+		this.fire.push([func, params]);
+	},
+	runFunctions: function() {
+		for (var i = 0; i < scrollDebounce.fire.length; i++) {
 			scrollDebounce.fire[i][0].apply(this, scrollDebounce.fire[i][1]);
 		}
 	},
-	didWindowScroll:function(){
+	didWindowScroll: function() {
 		scrollDebounce.didScroll = true;
 	},
- 	init:function(){
- 		this.$the_window = window;
-       	scrollDebounce.$the_window.onscroll = scrollDebounce.didWindowScroll; 
-        setInterval(function(){
-        	if(scrollDebounce.didScroll){
-        		scrollDebounce.didScroll = false;
-        		scrollDebounce.runFunctions();
-        	}
-        },scrollDebounce.pause);
- 	}
+	init: function() {
+		window.onscroll = scrollDebounce.didWindowScroll;
+		setInterval(function() {
+			if (scrollDebounce.didScroll) {
+				scrollDebounce.didScroll = false;
+				scrollDebounce.runFunctions();
+			}
+		}, scrollDebounce.pause);
+	}
 };
 module.exports = scrollDebounce;
